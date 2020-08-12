9cedff699ab9dd2a46e02d6b36e3b1c829683fbb
William <william.fischer42@gmail.com>
2020-08-12 13:06:44 -0600

    Add vue-bit project to consume shared component

---
+++ bit-vue/vue-bit-consumer/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ProductList />
+  </div>
+</template>
+
+<script>
+import ProductList from '@bit/wfischer42.vue-tutorial.product-list'
+
+export default {
+  name: 'app',
+  components: {
+    ProductList
+  }
+}
+</script>
