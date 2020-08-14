6c5cace28f281b3fbba453a1d74cc0d1aeac49dd
William <william.fischer42@gmail.com>
2020-08-14 14:15:44 -0600

    Change prisma datamodel

--- prisma-1-setup/index.js
+++ prisma-1-setup/index.js
@@ -2,13 +2,32 @@ const { prisma } = require('./generated/prisma-client')
 
 // A `main` function so that we can use async/await
 async function main() {
-  // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: 'Alice' })
+  // Create a new user with a new post
+  const newUser = await prisma.createUser({
+    name: 'Bob',
+    email: 'bob@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Join us for GraphQL Conf in 2019',
+        },
+        {
+          title: 'Subscribe to GraphQL Weekly for GraphQL news',
+        },
+      ],
+    },
+  })
   console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
 
   // Read all users from the database and print them to the console
   const allUsers = await prisma.users()
   console.log(allUsers)
+
+  const allPosts = await prisma.posts()
+  console.log(allPosts)
+
+  const postsByUser = await prisma.user({ email: 'bob@prisma.io' }).posts()
+  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`)
 }
 
-main().catch((e) => console.error(e))
+main().catch(e => console.error(e))
